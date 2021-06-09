68e32c7f679e2b15ae1629cf4204186d42e3dc46
ShunheWang <maisonwangsh@gmail.com>
2021-06-09 14:35:54 +0800

    4-7 delete 'HelloWorld' component codes

---
+++ web/src/components/the-footer.vue
@@ -0,0 +1,14 @@
+<template>
+    <a-layout-footer style="text-align: center">
+        Wiki E-book Project via Maison
+    </a-layout-footer>
+</template>
+
+
+<script lang="ts">
+    import { defineComponent } from 'vue';
+
+    export default defineComponent({
+        name: 'the-footer'
+    });
+</script>
