13d387740a660434ba4df6f810bd4285580ae9a2
ShunheWang <maisonwangsh@gmail.com>
2021-06-09 12:49:26 +0800

    4-3 Generate original web project via vue-cli

--- src/main/java/com/maison/wiki/service/EbookService.java
+++ src/main/java/com/maison/wiki/service/EbookService.java
@@ -5,6 +5,7 @@ import com.maison.wiki.entity.EbookExample;
 import com.maison.wiki.mapper.EbookMapper;
 import com.maison.wiki.req.EbookReq;
 import com.maison.wiki.resp.EbookResp;
+import com.maison.wiki.utils.CopyUtil;
 import org.springframework.beans.BeanUtils;
 import org.springframework.stereotype.Service;
 
@@ -24,13 +25,17 @@ public class EbookService {
         EbookExample.Criteria criteria = ebookExample.createCriteria();
         criteria.andNameLike("%" + req.getName() + "%");
         List<Ebook> ebookList = ebookMapper.selectByExample(ebookExample);
-        ArrayList<EbookResp> respList = new ArrayList<>();
-        for (Ebook ebook : ebookList) {
-            EbookResp ebookResp = new EbookResp();
-            BeanUtils.copyProperties(ebook,ebookResp);
-            respList.add(ebookResp);
-        }
-        return respList;
+//        ArrayList<EbookResp> respList = new ArrayList<>();
+//        for (Ebook ebook : ebookList) {
+////            EbookResp ebookResp = new EbookResp();
+////            BeanUtils.copyProperties(ebook,ebookResp);
+//            单个复制
+//            EbookResp ebookResp = CopyUtil.copy(ebook, EbookResp.class);
+//            respList.add(ebookResp);
+//        }
+//        列表复制
+        List<EbookResp> list = CopyUtil.copyList(ebookList, EbookResp.class);
+        return list;
     }
 
 }
