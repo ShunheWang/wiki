8f95a2dd74153e48071de671144960be5eb47f04
ShunheWang <maisonwangsh@gmail.com>
2021-06-22 14:59:21 +0800

    6-2 add ebook page and new pages, including pages, routers, nav as well

--- web/src/components/the-header.vue
+++ web/src/components/the-header.vue
@@ -1,14 +1,16 @@
 <template>
     <a-layout-header class="header">
         <div class="logo" />
-        <a-menu
-                theme="dark"
-                mode="horizontal"
-                :style="{ lineHeight: '64px' }"
-        >
-            <a-menu-item key="1">nav 1</a-menu-item>
-            <a-menu-item key="2">nav 2</a-menu-item>
-            <a-menu-item key="3">nav 3</a-menu-item>
+        <a-menu theme="dark" mode="horizontal" :style="{ lineHeight: '64px' }">
+            <a-menu-item key="home">
+                <router-link to="/">home</router-link>
+            </a-menu-item>
+            <a-menu-item key="2">
+                <router-link to="/admin/ebook">ebook manage</router-link>
+            </a-menu-item>
+            <a-menu-item key="3">
+                <router-link to="/about">about me</router-link>
+                </a-menu-item>
         </a-menu>
     </a-layout-header>
 </template>
