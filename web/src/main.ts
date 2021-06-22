0b6133a5f5281ff0c5cad785064d82e9f47b4664
ShunheWang <maisonwangsh@gmail.com>
2021-06-22 13:53:08 +0800

    5-6 axios 拦截器打印请求参数和返回参数

--- web/src/main.ts
+++ web/src/main.ts
@@ -9,6 +9,27 @@ import axios from 'axios';
 
 axios.defaults.baseURL = process.env.VUE_APP_SERVER;
 
+/**
+ * axios 拦截器
+ */
+axios.interceptors.request.use(function (config) {
+    console.log("request parameter: ", config);
+    return config;
+},error => {
+    console.log("request error: ", error);
+    return Promise.reject(error);
+});
+
+axios.interceptors.response.use(function (response) {
+    console.log("response result: ", response);
+    return response;
+}, error =>  {
+    console.log("response error: ", error);
+    return Promise.reject(error);
+});
+
+
+
 const app = createApp(App);
 app.use(store).use(router).use(Antd).mount('#app');
 
