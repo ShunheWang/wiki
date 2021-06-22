2f7302264952c2f461449fa4bc2a8cff4a399c46
ShunheWang <maisonwangsh@gmail.com>
2021-06-22 15:35:18 +0800

    6-3 修改电子书管理页面布局

--- web/src/views/admin/admin-ebook.vue
+++ web/src/views/admin/admin-ebook.vue
@@ -1,5 +1,11 @@
 <template>
-    <div class="about">
-        ebook management
-    </div>
+    <a-layout>
+        <a-layout-content :style="{ background: '#fff', padding: '24px', margin: 0, minHeight: '280px' }">
+            <a-list item-layout="vertical" size="large" :data-source="ebooks" :grid="{ gutter: 20, column:3 }">
+                <div class="about">
+                    ebook management
+                </div>
+            </a-list>
+        </a-layout-content>
+    </a-layout>
 </template>
