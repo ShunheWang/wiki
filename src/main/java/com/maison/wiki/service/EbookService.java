52dab2ba02bfda35635e11961f32c6d1779848b2
ShunheWang <maisonwangsh@gmail.com>
2021-06-27 20:14:46 +0800

    6-4 add pagehelper & modify mapper log status

--- src/main/java/com/maison/wiki/service/EbookService.java
+++ src/main/java/com/maison/wiki/service/EbookService.java
@@ -1,11 +1,15 @@
 package com.maison.wiki.service;
 
+import com.github.pagehelper.PageHelper;
+import com.github.pagehelper.PageInfo;
 import com.maison.wiki.entity.Ebook;
 import com.maison.wiki.entity.EbookExample;
 import com.maison.wiki.mapper.EbookMapper;
 import com.maison.wiki.req.EbookReq;
 import com.maison.wiki.resp.EbookResp;
 import com.maison.wiki.utils.CopyUtil;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.BeanUtils;
 import org.springframework.stereotype.Service;
 import org.springframework.util.ObjectUtils;
@@ -21,13 +25,21 @@ public class EbookService {
     @Resource
     private EbookMapper ebookMapper;
 
+    private static final Logger LOG = LoggerFactory.getLogger(EbookService.class);
+
     public List<EbookResp> list(EbookReq req){
         EbookExample ebookExample = new EbookExample();
         EbookExample.Criteria criteria = ebookExample.createCriteria();
         if (!ObjectUtils.isEmpty(req.getName())) {
             criteria.andNameLike("%" + req.getName() + "%");
         }
+        PageHelper.startPage(1,3);
         List<Ebook> ebookList = ebookMapper.selectByExample(ebookExample);
+
+        PageInfo<Ebook> pageInfo = new PageInfo<>(ebookList);
+        LOG.info("page total => {}" + pageInfo.getTotal());
+        LOG.info("page pages => {}" + pageInfo.getPages());
+
 //        ArrayList<EbookResp> respList = new ArrayList<>();
 //        for (Ebook ebook : ebookList) {
 ////            EbookResp ebookResp = new EbookResp();
--- web/src/views/admin/admin-ebook.vue
+++ web/src/views/admin/admin-ebook.vue
@@ -1,11 +1,117 @@
 <template>
     <a-layout>
         <a-layout-content :style="{ background: '#fff', padding: '24px', margin: 0, minHeight: '280px' }">
-            <a-list item-layout="vertical" size="large" :data-source="ebooks" :grid="{ gutter: 20, column:3 }">
-                <div class="about">
-                    ebook management
-                </div>
-            </a-list>
+            <a-table :columns="columns" :row-key="record => record.id" :data-source="ebooks" :pagination="pagination" :loading="loading" @change="handleTableChange">
+                <template #cover="{text:cover}">
+                    <img v-if="cover" :src="cover" alt="avatar" />
+                </template>
+                <template v-slot:action="{text,record}">
+                    <a-space size="small">
+                        <a-button type="primary">
+                            modify
+                        </a-button>
+                        <a-button type="danger">
+                            delete
+                        </a-button>
+                    </a-space>
+                </template>
+            </a-table>
         </a-layout-content>
     </a-layout>
 </template>
+
+<script lang="ts">
+    import { defineComponent, onMounted, ref} from 'vue';
+    import axios from 'axios';
+    import _default from "ant-design-vue/es/vc-pagination/Pager";
+
+    export default defineComponent({
+        name: 'AdminEbook',
+        setup() {
+            const ebooks = ref();
+            const pagination = ref({
+                current: 1,
+                pageSize: 2,
+                total: 0
+            });
+            const loading = ref(false);
+            const columns = [
+                {
+                    title: '封面',
+                    dataIndex: 'cover',
+                    slots: {customRender: 'cover'}
+                },
+                {
+                    title: '名称',
+                    dataIndex: 'name'
+                },
+                {
+                    title: '分类一',
+                    key: 'category1Id',
+                    dataIndex: 'category1id',
+                },
+                {
+                    title: '分类二',
+                    dataIndex: 'category2id',
+                },
+                {
+                    title: '文档数',
+                    dataIndex: 'docCount',
+                },
+                {
+                    title: '阅读数',
+                    dataIndex: 'viewCount',
+                },
+                {
+                    title: '点赞数',
+                    dataIndex: 'voteCount',
+                },
+                {
+                    title: 'Action',
+                    key: 'action',
+                    slots: {customRender: 'action'}
+                },
+            ];
+
+            /**
+             * data query
+             * @param params
+             */
+            const handleQuery = (params: any) => {
+              loading.value = true;
+              axios.get("/ebook/list", params).then((response) => {
+                 loading.value = false;
+                 const data = response.data;
+                 ebooks.value = data.content;
+
+                 // 重置分页按钮
+                  pagination.value.current = params.page;
+              });
+            };
+
+            /**
+             * 表格点击页码时触发
+             */
+            const handleTableChange = (pagination: any) => {
+              console.log("page params -> " + pagination);
+              handleQuery({
+                  page: pagination.current,
+                  size: pagination.pageSize
+              });
+            };
+
+            onMounted(() => {
+               handleQuery({});
+            });
+
+            return {
+                ebooks,
+                pagination,
+                columns,
+                loading,
+                handleTableChange
+            }
+        }
+
+    });
+</script>
